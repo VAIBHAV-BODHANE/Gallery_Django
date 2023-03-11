cc89bfaa2b80c770ad10a4386842dd5c638b5de3
Vaibhav Bodhane <22360595+VAIBHAV-BODHANE@users.noreply.github.com>
2023-03-11 20:28:02 +0530

    Cors added

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'gallery_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -144,3 +146,8 @@ AUTH_USER_MODEL = 'gallery_app.UserProfile'
 # MEDIA Conf
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# CORS Conf
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
